68b6b13e87b2702d855ff9ad2dd5063c6743b0e4
Mahmoud <mahmoud.ped@gmail.com>
2023-07-26 15:32:42 +0300

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxDatatableComponent } from './ngx-datatable/ngx-datatable.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxDatatableComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
