7fe9dfa578fc606b6c52e6427dcdc79714ec827d
Mahmoud <mahmoud.ped@gmail.com>
2023-07-27 11:04:20 +0300

    1.1

---
+++ hrFront/src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxDatatableComponent } from './ngx-datatable/ngx-datatable.component';
+import { NgSelectComponent } from './ng-select/ng-select.component';
+import { InputComponent } from './input/input.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
+
+
+@NgModule({
+  declarations: [
+    NgxDatatableComponent,
+    NgSelectComponent,
+    InputComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgSelectModule,
+    ReactiveFormsModule,
+    NgxDatatableModule
+  ]
+})
+export class SharedModule { }
